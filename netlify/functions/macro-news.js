3a030a2ed341571a3b00a5c1efcfba50c1fee9a5
deepcatking-ux <deep.catking@gmail.com>
2026-07-20 15:50:04 +0530

    Initial commit

--- netlify/functions/macro-news.js
+++ netlify/functions/macro-news.js
@@ -125,7 +125,7 @@ exports.handler = async function (event) {
     }
 
     const apiToken = getEnv('MARKETAUX_API_KEY')
-    const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10)
+    const sevenDaysAgo = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10)
     const url =
       'https://api.marketaux.com/v1/news/all' +
       `?api_token=${apiToken}` +
